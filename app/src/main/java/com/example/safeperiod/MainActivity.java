c22ebec36598ec1ce8ec6ed550b6f51f944c509d
PAULI <adenreleademola@yahoo.com>
2019-05-10 13:19:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/safeperiod/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.safeperiod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
